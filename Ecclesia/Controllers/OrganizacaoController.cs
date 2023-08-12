a30d4b0eac35fde62f9c6a2ab51d9a1f0866f64c
Clébio Lima de Freitas <premiersoft-clebio.freitas@ailos.coop.br>
2023-08-12 17:53:23 -0300

    Incluindo OrganizacaoController

---
+++ Ecclesia/Controllers/OrganizacaoController.cs
@@ -0,0 +1,127 @@
+﻿using Ecclesia.Api.Dto;
+using Ecclesia.Domain;
+using Ecclesia.Service.Contracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecclesia.Api.Controllers
+{
+    public class OrganizacaoController : ControllerBase
+    {
+        private readonly IOrganizacaoService _service;
+
+        public OrganizacaoController(IOrganizacaoService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost, Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Route("api/[controller]")]
+        public async Task<IActionResult> Cadastrar([FromBody] OrganizacaoInsertDto dto)
+        {
+            try
+            {
+                await _service.Insert(
+                new Organizacao
+                {
+                    Nome = dto.Nome,
+                    Cnpj = dto.Cnpj,
+                    UsuarioCriacao = dto.Usuario
+                });
+
+                return Ok(new { Success = true });
+            }
+            catch (BusinessHttpResponseException ex)
+            {
+                return StatusCode((int)ex.Response.StatusCode, new { Succes = false, Response = ex.Response });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Succes = false, Error = ex.Message });
+            }
+        }
+
+        [HttpPut, Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Route("api/[controller]")]
+        public async Task<IActionResult> Atualizar([FromBody] OrganizacaoUpdateDto dto)
+        {
+            try
+            {
+                await _service.Update(
+                new Organizacao
+                {
+                    Id = dto.Id,
+                    Nome = dto.Nome,
+                    Cnpj = dto.Cnpj,
+                    UsuarioUltimaAlteracao = dto.Usuario,
+                    Status = dto.Status,
+                });
+                return Ok(new { Success = true });
+            }
+            catch (BusinessHttpResponseException ex)
+            {
+                return StatusCode((int)ex.Response.StatusCode, new { Succes = false, Response = ex.Response });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Succes = false, Error = ex.Message });
+            }
+        }
+
+        [HttpGet, Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Route("api/[controller]/{id}")]
+        public async Task<IActionResult> Consultar(int id)
+        {
+            try
+            {
+                return Ok(await _service.Get(id));
+            }
+            catch (BusinessHttpResponseException ex)
+            {
+                return StatusCode((int)ex.Response.StatusCode, new { Succes = false, Response = ex.Response });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Succes = false, Error = ex.Message });
+            }
+        }
+
+        [HttpGet, Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Route("api/[controller]/GetAll/{nome}")]
+        public async Task<IActionResult> Buscar(string nome)
+        {
+            try
+            {
+                return Ok(await _service.GetAll(nome));
+            }
+            catch (BusinessHttpResponseException ex)
+            {
+                return StatusCode((int)ex.Response.StatusCode, new { Succes = false, Response = ex.Response });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Succes = false, Error = ex.Message });
+            }
+        }
+
+        [HttpDelete, Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Route("api/[controller]/{id}")]
+        public async Task<IActionResult> Deletar(int id)
+        {
+            try
+            {
+                await _service.Delete(id);
+                return Ok(new { Success = true });
+            }
+            catch (BusinessHttpResponseException ex)
+            {
+                return StatusCode((int)ex.Response.StatusCode, new { Succes = false, Response = ex.Response });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Succes = false, Error = ex.Message });
+            }
+        }
+    }
+}
---
+++ Repository/Repositories/OrganizacaoRepository.cs
@@ -0,0 +1,94 @@
+﻿using Dapper;
+using Ecclesia.Domain;
+using Ecclesia.Repository.Contracts;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecclesia.Repository.Repositories
+{
+    public class OrganizacaoRepository : BaseRepository, IOrganizacaoRepository
+    {
+        public OrganizacaoRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public async Task Delete(int id)
+        {
+            #region sql
+            var sql = $@"
+                delete from organizacao where id = @id
+            ";
+            #endregion
+
+            await _connection.ExecuteAsync(sql, new { Id = id });
+        }
+
+        public async Task<Organizacao> Get(int id)
+        {
+            #region sql
+            var sql = $@"
+                select * from organizacao where id = @id
+            ";
+            #endregion
+
+            var reg = await _connection.QueryAsync<Organizacao>(sql, new { Id = id });
+            return (Organizacao)reg.FirstOrDefault();
+        }
+
+        public async Task<List<Organizacao>> GetAll(string nome)
+        {
+            var like = $"%{nome}%";
+            #region sql
+            var sql = $@"
+                select * from organizacao where nome like @like
+            ";
+            #endregion
+
+            var regs = await _connection.QueryAsync<Organizacao>(sql, new { Like = like });
+            return (List<Organizacao>)regs;
+        }
+
+        public async Task Insert(Organizacao reg)
+        {
+            #region sql
+            var sql = $@"
+                insert into organizacao 
+                (   
+                    nome,
+                    cnpj,
+                    datacriacao,
+                    usuariocriacao                  
+                ) values 
+                (
+                    @nome,
+                    @cnpj,                
+                    now(),
+                    @usuariocriacao        
+                );
+            ";
+            #endregion
+
+            await _connection.ExecuteAsync(sql, reg);
+        }
+
+        public async Task Update(Organizacao reg)
+        {
+            #region sql
+            var sql = $@"
+               update organizacao 
+               set nome = @nome,
+                   cnpj = @cnpj,
+                   usuarioultimaalteracao = @usuarioultimaalteracao,
+                   dataultimaalteracao = now()
+               where id = @id
+            ";
+            #endregion
+
+            await _connection.ExecuteAsync(sql, reg);
+        }
+    }
+}
